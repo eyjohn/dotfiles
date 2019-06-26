e4dac83fa50a9987ebc296f2fbaeaef3bfc99d5d
Evgeny Yakimov <john.yakimov@gmail.com>
2019-06-26 22:11:02 +0100

    Add url launcher + fix wsl mount
    Gnome-terminal url launching works via host chrome.
    
    Changed wsl mount point back to /mnt/ (default).

--- wsl/install.sh
+++ wsl/install.sh
@@ -7,7 +7,7 @@ fi
 if confirm "Installing ssh startup on launch? [y/n]: "; then
   sudo apt -y purge openssh-server
   sudo apt -y install openssh-server
-  sudo cp ~/.dotfiles/wsl/usr/local/bin/start_ssh /usr/local/bin/
+  sudo cp -p ~/.dotfiles/wsl/usr/local/bin/start_ssh /usr/local/bin/
   sudo cp ~/.dotfiles/wsl/etc/sudoers.d/start_ssh /etc/sudoers.d/
   sudo /usr/local/bin/start_ssh
   cp -asvb ~/.dotfiles/wsl/.profile.d/22_wsl_ssh_server ~/.profile.d/
@@ -18,4 +18,12 @@ if confirm "Installing gnome-terminal? [y/n]: "; then
   sudo systemd-machine-id-setup
   cp -asvb ~/.dotfiles/wsl/.profile.d/90_wsl_gnome_terminal ~/.profile.d/
   echo "NOTE: Make sure you've installed VcXsrv - https://sourceforge.net/projects/vcxsrv/"
+fi
+
+if confirm "Set chrome.exe as launcher for gnome-terminal? [y/n]: "; then
+  sudo apt -y install libglib2.0-bin
+  # Copy chrome.desktop
+  cp -asvb ~/.dotfiles/wsl/.local ~/
+  gio mime x-scheme-handler/http chrome.desktop
+  gio mime x-scheme-handler/https chrome.desktop
 fi
