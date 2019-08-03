0b22093e382e67897a02ceaa97aae33af489ef71
Evgeny Yakimov <john.yakimov@gmail.com>
2019-08-03 08:14:00 +0100

    WSL-Docker-Minikube mount dir

--- wsl/install.sh
+++ wsl/install.sh
@@ -28,8 +28,9 @@ if confirm "Set chrome.exe as launcher for gnome-terminal? [y/n]: "; then
   gio mime x-scheme-handler/https chrome.desktop
 fi
 
-if confirm "Install minikube.exe helpers ssh/minikube-env? [y/n]: "; then
+if confirm "Install minikube.exe helpers ssh/minikube-env/mount? [y/n]: "; then
   cp -asvi ~/.dotfiles/wsl/bin/minikube-env ~/bin/
+  cp -asvi ~/.dotfiles/wsl/bin/minikube-mount ~/bin/
   cp -asvi ~/.dotfiles/wsl/.ssh/config.d/minikube ~/.ssh/config.d/
   echo 'NOTE: Install choco/minikube on Windows'
   echo 'NOTE: Add windows user to "Hyper-V Administrators"'
