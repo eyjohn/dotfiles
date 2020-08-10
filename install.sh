0ef89d58f8e58ee29e11f3e2ea5c245dbc4775ca
Evgeny Yakimov <john.yakimov@gmail.com>
2020-08-10 08:28:01 +0100

    Set git remote url to ssh variant

--- install.sh
+++ install.sh
@@ -52,6 +52,11 @@ if confirm "Install kubectl autocomplete? [y/n]: "; then
   cp -asvb ~/.dotfiles/.profile.d/18_kubectl_autocomplete ~/.profile.d/
 fi
 
+if git --git-dir "$BASE/.git" remote get-url origin | grep -q ://github.com && confirm "Patch git remote to ssh variant? [y/n]:"; then 
+  GIT_REMOTE=github:$(git remote get-url origin | sed 's%.*://github\.com/\(.*\)\.git%\1%')
+  git --git-dir "$BASE/.git" remote set-url origin "$GIT_REMOTE"
+fi
+
 # WSL Specifics
 WSL_VERSION=$(wsl.exe -l -v | sed $'s/[^[:print:]\t]//g' | grep -a '[*]' | awk '{print $4}')
 if [ -n "$WSL_VERSION" ]; then
