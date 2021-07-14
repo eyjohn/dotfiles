91a884271e40fd5f461b2ff88361b7cd310ad596
Evgeny Yakimov <john.yakimov@gmail.com>
2021-07-14 18:20:41 +0100

    Add git recent/recentout + formatting
    - Can now view and checkout recent git branches
    - These are installed in "bin"

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Installing dotfiles"
 
-BASE="$( cd "$(dirname "$0")" ; pwd -P )"
+BASE="$(
+  cd "$(dirname "$0")"
+  pwd -P
+)"
 
 source "$BASE/functions.sh"
 source "$BASE/wsl/functions.sh"
@@ -19,20 +22,23 @@ mkdir -p ${DIRS[@]/#/~/}
 TARGETS=(.bash_aliases .gitconfig .gitignore_global)
 cp -asvb ${TARGETS[@]/#/~/.dotfiles/} ~/
 
+BIN_TARGETS=(git-recent git-recentout)
+cp -asvb ${BIN_TARGETS[@]/#/~/.dotfiles/bin/} ~/bin/
+
 # .ssh/config.d doesn't support backups
 cp -asvi ~/.dotfiles/.ssh/config.d/github ~/.ssh/config.d/
 
-[ ! -f ~/.ssh/config ] && echo > ~/.ssh/config # Add blank line for prepend to work
+[ ! -f ~/.ssh/config ] && echo >~/.ssh/config # Add blank line for prepend to work
 prepend_file ~/.ssh/config 'Include config.d/*'
 insert_file ~/.profile 'for f in ~/.profile.d/*[^~]; do source $f; done'
 insert_file ~/.bashrc 'for f in ~/.bashrc.d/*[^~]; do source $f; done'
 
-if grep -q '#force_color_prompt=yes' ~/.bashrc; then 
+if grep -q '#force_color_prompt=yes' ~/.bashrc; then
   echo "Patching .bashrc to force_color_prompt=yes"
   sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
 fi
 
-if git --git-dir "$BASE/.git" remote get-url origin | grep -q ://github.com && confirm "Patch git remote to ssh variant? [y/n]:"; then 
+if git --git-dir "$BASE/.git" remote get-url origin | grep -q ://github.com && confirm "Patch git remote to ssh variant? [y/n]:"; then
   GIT_REMOTE=github:$(git remote get-url origin | sed 's%.*://github\.com/\(.*\)\.git%\1%')
   git --git-dir "$BASE/.git" remote set-url origin "$GIT_REMOTE"
 fi
@@ -55,11 +61,11 @@ if confirm "Install kubectl? [y/n]: "; then
   sudo apt-get install -y kubectl
 fi
 
-if which kubectl > /dev/null && confirm "Install kubectl autocomplete? [y/n]: "; then
+if which kubectl >/dev/null && confirm "Install kubectl autocomplete? [y/n]: "; then
   cp -asvb ~/.dotfiles/.bashrc.d/18_kubectl_autocomplete ~/.bashrc.d/
 fi
 
-if which kubectl > /dev/null && confirm "Set KUBECONFIG=~/.kube/* [y/n]: "; then
+if which kubectl >/dev/null && confirm "Set KUBECONFIG=~/.kube/* [y/n]: "; then
   cp -asvb ~/.dotfiles/.profile.d/19_kubectl_kubeconfig ~/.profile.d/
 fi
 
@@ -71,10 +77,10 @@ if confirm "Install helm? [y/n]: "; then
 fi
 
 if confirm "Install brig (Brigade CLI)? [y/n]: "; then
-  curl -s https://api.github.com/repos/brigadecore/brigade/releases/latest \
-    | grep "browser_download_url.*linux-amd64" \
-    | cut -d '"' -f 4 \
-    | wget -i - -O ~/bin/brig
+  curl -s https://api.github.com/repos/brigadecore/brigade/releases/latest |
+    grep "browser_download_url.*linux-amd64" |
+    cut -d '"' -f 4 |
+    wget -i - -O ~/bin/brig
   chmod +x ~/bin/brig
   cp -asvb ~/.dotfiles/.profile.d/17_brigade_namespace ~/.profile.d/
 fi
